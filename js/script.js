6d48566b10e7da01ecfd100b9d36bb8052646390
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-01-23 16:35:48 +0100

    add array data

--- js/script.js
+++ js/script.js
@@ -17,18 +17,21 @@ Creare un filtro di ricerca in tempo reale
 
 */
 
-
-
-
-
-
 console.log('Vue on',Vue)
 
 const {createApp} = Vue;
 
 const app = createApp ({  
-    
-         
+    data:() => ({
+            
+    tasks: [
+        { id: 1, done: false, text: 'Fare la spesa' },
+        { id: 2, done: true, text: 'Lavare i capelli' },
+        { id: 3, done: false, text: 'Comprare una marca da bollo' },
+        { id: 4, done: false, text: 'Aggiornare il PC' }
+    ],
+    }),
+             
 })
 
 app.mount('#root')
